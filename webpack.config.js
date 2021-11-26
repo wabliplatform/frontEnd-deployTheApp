a225791275aabfd31fc8c92c7f8ff7cc3c7edf58
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2021-11-26 13:04:19 +0200

    My commit message

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+	'homePage' : './javascript/homePage.js',
+},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    
+  },
+};
